9f728763db7b78a5c49c37c500127e81538c70c3
Abraham <abrahamd@hotmail.com>
2018-03-14 20:56:53 -0400

    sqlite db

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+using campgrounds_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace campgrounds_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
